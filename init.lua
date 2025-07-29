bf78e66e7227d0922b66302a9ac0d3728105f8e9
Yiannis Belias <yiannisbe@gmail.com>
2025-07-29 14:49:10 +0300

    Set some personal defaults

--- init.lua
+++ init.lua
@@ -98,11 +98,17 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Custom yiannis options
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -168,6 +174,11 @@ vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+
+-- Custom yiannis keymaps
+vim.keymap.set('n', 't', ':tabnew<CR>')
+vim.keymap.set('n', ',', ':tabp<CR>')
+vim.keymap.set('n', '.', ':tabn<CR>')
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
